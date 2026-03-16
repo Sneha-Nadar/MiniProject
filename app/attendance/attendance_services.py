1caa074777f4a7a94f8317c9572edafba90ac686
Sneha-Nadar <snehanadar16@gmail.com>
2026-03-16 11:49:05 +0530

    commit

--- app/attendance/attendance_services.py
+++ app/attendance/attendance_services.py
@@ -4,52 +4,67 @@ from datetime import datetime
 
 FILE_NAME = "attendance.xlsx"
 
+
+def get_current_lecture():
+
+    now = datetime.now().time()
+
+    lectures = [
+        ("L1","08:30","09:30"),
+        ("L2","09:30","10:30"),
+        ("L3","10:30","11:30"),
+        ("L4","11:30","12:30"),
+        ("L5","01:30","02:30"),
+        ("L6","02:30","03:30"),
+        ("L7","03:30","04:30")
+    ]
+
+    for lecture,start,end in lectures:
+
+        start = datetime.strptime(start,"%H:%M").time()
+        end = datetime.strptime(end,"%H:%M").time()
+
+        if start <= now <= end:
+            return lecture
+
+    return None
+
+
 def mark_attendance(name):
 
+    lecture = get_current_lecture()
+
+    if lecture is None:
+        print("Outside lecture time")
+        return
+
     now = datetime.now()
+
     date = now.strftime("%Y-%m-%d")
     time = now.strftime("%H:%M:%S")
 
     if os.path.exists(FILE_NAME):
         df = pd.read_excel(FILE_NAME)
     else:
-        df = pd.DataFrame(columns=["Name", "Date", "Time"])
+        df = pd.DataFrame(columns=["Name","Date","Time","Lecture"])
 
-    if ((df["Name"] == name) & (df["Date"] == date)).any():
+
+    if ((df["Name"] == name) &
+        (df["Date"] == date) &
+        (df["Lecture"] == lecture)).any():
+
         return
 
-    df.loc[len(df)] = [name, date, time]
-
-    df.to_excel(FILE_NAME, index=False)
-
-    print(f"✅ Attendance marked for {name}")
-from datetime import datetime
-
-FILE_NAME = "attendance.xlsx"
-
-def mark_attendance(name):
-
-    now = datetime.now()
-    date = now.strftime("%Y-%m-%d")
-    time = now.strftime("%H:%M:%S")
-
-    if os.path.exists(FILE_NAME):
-        df = pd.read_excel(FILE_NAME)
-    else:
-        df = pd.DataFrame(columns=["Name", "Date", "Time"])
-
-    # prevent duplicate attendance
-    if ((df["Name"] == name) & (df["Date"] == date)).any():
-        return
 
     new_row = {
         "Name": name,
         "Date": date,
-        "Time": time
+        "Time": time,
+        "Lecture": lecture
     }
 
-    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    df = pd.concat([df,pd.DataFrame([new_row])],ignore_index=True)
 
-    df.to_excel(FILE_NAME, index=False)
+    df.to_excel(FILE_NAME,index=False)
 
-    print(f"✅ Attendance marked for {name}")
+    print(f"Attendance marked for {name} ({lecture})")
